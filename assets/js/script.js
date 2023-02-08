d2a5c4087e47d81300c0b890774c68c84ddd29da
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-02-08 17:45:52 -0500

    added pseudocode for javascript to break down steps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
 // Assignment Code
+// need 1. click interaction generates, need series of prompts for pw criteria
+//      2. options of criteria to select. 
+//      3. pw length (at least 8 less than 128);
+//      4. confirm types of character 
+//          - (prompt whether or not to include lowercase, uppercase, numeric, and/or special characters)
+//      5. after EACH prompt is answered, answer is validated (alert and return false)
+//      6. at least one must be selected (return or alert or loop back up)
+//      7. after all prompts answered, password is generated (random ?)
+//      8. password should show up as alert or on page
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
