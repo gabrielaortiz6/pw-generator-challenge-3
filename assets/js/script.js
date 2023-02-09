10a1b68a14e3491824729a173603a3c772ebe38c
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-02-09 15:55:34 -0500

    created generatePassword function and included prompt for character length, began working on other prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-// need 1. click interaction generates, need series of prompts for pw criteria
+// need 1. click generates interaction then prompts for pw criteria
 //      2. options of criteria to select. 
 //      3. pw length (at least 8 less than 128);
 //      4. confirm types of character 
@@ -9,7 +9,26 @@
 //      7. after all prompts answered, password is generated (random ?)
 //      8. password should show up as alert or on page
 
+// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
+
+//function for password criteria prompts // could be generateBtn.prompt idk
+
+var generatePassword = function() {
+  var passwordUserInput = window.prompt("Please enter desired length of characters for password (between 8 and 128 characters");
+  if (passwordUserInput < 8 || passwordUserInput >= 128) {
+    alert("Please choose length of at least 8 characters and no more than 128 characters");
+    generatePassword();
+  } else if (!passwordUserInput) {
+  alert("Please choose length of at least 8 characters and no more than 128 characters");
+  return;
+  //for the next criteria - should I do nested function? set a new variable to store user input ??
+  } else if (passwordUserInput >= 8 && passwordUserInput <= 128) {
+    var lowerCase = confirm("Thank you. Do you want your password to include lowercase letters?");
+
+  }
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -20,5 +39,6 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+//Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
